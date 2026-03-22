0f330e962691e133d2089d7094743b731b6141ba
Agnibha Debnath <agnibhadebnath@gmail.com>
2026-03-22 23:59:19 +0530

    Initial commit

---
+++ app/About/page.tsx
@@ -0,0 +1,13 @@
+
+ const page = () => {
+  return (
+      <div className='flex justify-center'>
+          <div className="py-4">
+              <h1 className='text-5xl font-bold'>About</h1>
+          </div>
+      </div>
+      
+  )
+}
+
+export default page;
---
+++ components/heading.tsx
@@ -0,0 +1,10 @@
+const Heading = () => {
+    return (
+      <div className="my-2 flex justify-center">
+            <div className="py-4">
+               <h1 className="font-bold text-7xl">Hi!, Agnibha</h1>
+            </div>
+         </div>
+  )
+}
+export default Heading ;
